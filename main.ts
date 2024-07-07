a7da588277bec4b1015cee5e892738e793f7995c
Sam White <samiwhite97@gmail.com>
2024-07-06 18:37:19 -0600

    initial c

---
+++ main.ts
@@ -0,0 +1,12 @@
+import { Plugin } from "obsidian";
+
+const quoteWebsiteAPI = "https://api.quotable.io";
+
+export default class QuotePlugin extends Plugin {
+  async onload() {
+    // Configure resources needed by the plugin.
+  }
+  async onunload() {
+    // Release any resources configured by the plugin.
+  }
+}
