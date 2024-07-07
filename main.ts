5367999a06b89e267abf76144f95e0c0b65fe85a
Sam White <samiwhite97@gmail.com>
2024-07-06 18:51:07 -0600

    Added a section to interface via the command line and then print to console

--- main.ts
+++ main.ts
@@ -5,6 +5,13 @@ const quoteWebsiteAPI = "https://api.quotable.io";
 export default class QuotePlugin extends Plugin {
   async onload() {
     // Configure resources needed by the plugin.
+    this.addCommand({
+      id: "insert-quote",
+      name: "Insert Quote",
+      callback: () => {
+        console.log("It worked!");
+      },
+    });
   }
   async onunload() {
     // Release any resources configured by the plugin.
